1d95b00ca463247ebcc0735d7c5ebd4f7f9996d0
piratelicorne <thecodeloops@gmail.com>
2020-11-14 20:50:39 -0500

    libft

---
+++ _newcursus/ft_printf/libft/libft.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhuerta <mhuerta@student.42.us.org>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/31 18:58:37 by mhuerta           #+#    #+#             */
+/*   Updated: 2020/11/09 17:08:56 by mhuerta          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <unistd.h>
+# include <stdlib.h>
+
+size_t				ft_strlen(const char *s);
+size_t          	ft_intlen(size_t n);
+int		            ft_uintlen(unsigned int n, unsigned int base);
+int					ft_isdigit(int c);
+void				ft_putchar(char c);
+void	            ft_putchar_fd(char c, int fd);
+void	            ft_putnbr(int nb);
+void	            ft_putnbr_fd(int n, int fd);
+void	            ft_putunbr(unsigned int nb);
+void				ft_putstr(char const *s);
+void	            ft_putstr_fd(char const *s, int fd);
+char				*ft_strsub(char const *s, unsigned int start, size_t len);
+char	            *ft_strnew(size_t size);
+
+
+#endif
