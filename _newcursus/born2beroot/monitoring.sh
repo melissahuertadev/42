5bec91e85b6e0316d464d17de58a45af42a69a50
piratelicorne <thecodeloops@gmail.com>
2021-10-03 03:12:43 -0500

    monitoring template

---
+++ _newcursus/born2beroot/monitoring.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+wall "
+    #Architecture: The architecture of your operating system and its kernel version.
+    #CPU physical :  The number of physical processors.
+    #vCPU :  The number of virtual processors.
+    #Memory Usage:  The current available RAM on your server and its utilization rate as a percentage.
+    #Disk Usage:  The current available memory on your server and its utilization rate as a percentage
+    #CPU load:  The current utilization rate of your processors as a percentage.
+    #Last boot:  The date and time of the last reboot.
+    #LVM use:  Whether LVM is active or not.
+    #Connexions TCP :  The number of active connections.
+    #User log: The number of users using the server.
+    #Network:  The IPv4 address of your server and its MAC (Media Access Control) address.
+    #Sudo :  The number of commands executed with the sudo program."
