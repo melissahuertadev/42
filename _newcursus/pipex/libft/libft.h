f82b046e0f16270982e5cce143c399faba700165
piratelicorne <thecodeloops@gmail.com>
2021-10-13 02:31:06 -0500

    libft essentials

---
+++ _newcursus/pipex/libft/libft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhuerta <mhuerta@student.42.us.org>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/31 18:58:37 by mhuerta           #+#    #+#             */
+/*   Updated: 2021/10/13 02:26:46 by mhuerta          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <unistd.h>
+# include <stdlib.h>
+
+void	            ft_putchar_fd(char c, int fd);
+void	            ft_putstr_fd(char const *s, int fd);
+
+
+#endif
